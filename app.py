de4573f48ccea59028fee1d42d042b52fe767352
nanorisrael <israel.nanor@compozelabs.com>
2023-10-19 12:28:17 +0000

    Add templates

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
+from markupsafe import escape
 
 
 app = Flask(__name__)
@@ -6,3 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
 	return "Hello World!"
+
+@app.route('/<path:name>')
+def hello_user(name):
+	return f"Hello {name}!"
+
+@app.route('/index.html')
+def hello_html(name=None):
+	return render_template('index.html', name=name)
